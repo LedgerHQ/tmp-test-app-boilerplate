d6de264c3a0af9dcf0421d273ccb484f80d81085
Xavier Chapron <xavier.chapron@ledger.fr>
2024-03-06 18:17:40 +0100

    src/ui/menu_nbgl.c: Use nbgl_useCaseHomeAndSettings()

--- src/ui/menu_nbgl.c
+++ src/ui/menu_nbgl.c
@@ -34,22 +34,6 @@ void app_quit(void) {
     os_sched_exit(-1);
 }
 
-// home page definition
-void ui_menu_main(void) {
-// This parameter shall be set to false if the settings page contains only information
-// about the application (version , developer name, ...). It shall be set to
-// true if the settings page also contains user configurable parameters related to the
-// operation of the application.
-#define SETTINGS_BUTTON_ENABLED (true)
-
-    nbgl_useCaseHome(APPNAME,
-                     &C_app_boilerplate_64px,
-                     NULL,
-                     SETTINGS_BUTTON_ENABLED,
-                     ui_menu_settings,
-                     app_quit);
-}
-
 //  -----------------------------------------------------------
 //  --------------------- SETTINGS MENU -----------------------
 //  -----------------------------------------------------------
@@ -69,55 +53,9 @@ static const nbgl_contentInfoList_t infoList = {
     .infoContents = INFO_CONTENTS,
 };
 
-// callback for setting warning choice
-static void review_warning_choice(bool confirm) {
-    uint8_t switch_value;
-    if (confirm) {
-        // toggle the switch value
-        switch_value = !N_storage.dummy2_allowed;
-        switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) N_storage.dummy2_allowed;
-        // store the new setting value in NVM
-        nvm_write((void*) &N_storage.dummy2_allowed, &switch_value, 1);
-    }
-
-    // return to the settings menu
-    ui_menu_settings();
-}
-
-static void controls_callback(int token, uint8_t index, int page) {
-    UNUSED(index);
-    UNUSED(page);
-
-    uint8_t switch_value;
-    if (token == DUMMY_SWITCH_1_TOKEN) {
-        // Dummy 1 switch touched
-        // toggle the switch value
-        switch_value = !N_storage.dummy1_allowed;
-        switches[DUMMY_SWITCH_1_ID].initState = (nbgl_state_t) N_storage.dummy1_allowed;
-        // store the new setting value in NVM
-        nvm_write((void*) &N_storage.dummy1_allowed, &switch_value, 1);
-    } else if (token == DUMMY_SWITCH_2_TOKEN) {
-        // Dummy 2 switch touched
-
-        // in this example we display a warning when the user wants
-        // to activate the dummy 2 setting
-        if (!N_storage.dummy2_allowed) {
-            // Display the warning message and ask the user to confirm
-            nbgl_useCaseChoice(&C_warning64px,
-                               "Dummy 2",
-                               "Are you sure to\nallow dummy 2\nin transactions?",
-                               "I understand, confirm",
-                               "Cancel",
-                               review_warning_choice);
-        } else {
-            // toggle the switch value
-            switch_value = !N_storage.dummy2_allowed;
-            switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) N_storage.dummy2_allowed;
-            // store the new setting value in NVM
-            nvm_write((void*) &N_storage.dummy2_allowed, &switch_value, 1);
-        }
-    }
-}
+static uint8_t initSettingPage;
+static void review_warning_choice(bool confirm);
+static void controls_callback(int token, uint8_t index, int page);
 
 // settings menu definition
 #define SETTING_CONTENTS_NB 1
@@ -136,10 +74,70 @@ static const nbgl_genericContents_t settingContents = {
     .nbContents = SETTING_CONTENTS_NB
 };
 
-void ui_menu_settings() {
-#define INIT_SETTINGS_PAGE   (0)
 
-    // Initialise switches data
+// callback for setting warning choice
+static void review_warning_choice(bool confirm) {
+    uint8_t switch_value;
+    if (confirm) {
+        // toggle the switch value
+        switch_value = !N_storage.dummy2_allowed;
+        switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) switch_value;
+        // store the new setting value in NVM
+        nvm_write((void*) &N_storage.dummy2_allowed, &switch_value, 1);
+    }
+
+    // Reset setting menu to the right page
+    nbgl_useCaseHomeAndSettings(APPNAME,
+                                &C_app_boilerplate_64px,
+                                NULL,
+                                &initSettingPage,
+                                &settingContents,
+                                &infoList,
+                                NULL,
+                                NULL,
+                                app_quit);
+}
+
+static void controls_callback(int token, uint8_t index, int page) {
+    UNUSED(index);
+
+    initSettingPage = page;
+
+    uint8_t switch_value;
+    if (token == DUMMY_SWITCH_1_TOKEN) {
+        // Dummy 1 switch touched
+        // toggle the switch value
+        switch_value = !N_storage.dummy1_allowed;
+        switches[DUMMY_SWITCH_1_ID].initState = (nbgl_state_t) switch_value;
+        // store the new setting value in NVM
+        nvm_write((void*) &N_storage.dummy1_allowed, &switch_value, 1);
+    } else if (token == DUMMY_SWITCH_2_TOKEN) {
+        // Dummy 2 switch touched
+
+        // in this example we display a warning when the user wants
+        // to activate the dummy 2 setting
+        if (!N_storage.dummy2_allowed) {
+            // Display the warning message and ask the user to confirm
+            nbgl_useCaseChoice(&C_warning64px,
+                               "Dummy 2",
+                               "Are you sure to\nallow dummy 2\nin transactions?",
+                               "I understand, confirm",
+                               "Cancel",
+                               review_warning_choice);
+        } else {
+            // toggle the switch value
+            switch_value = !N_storage.dummy2_allowed;
+            switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) switch_value;
+            // store the new setting value in NVM
+            nvm_write((void*) &N_storage.dummy2_allowed, &switch_value, 1);
+        }
+    }
+}
+
+// home page definition
+void ui_menu_main(void) {
+
+    // Initialize switches data
     switches[DUMMY_SWITCH_1_ID].initState = (nbgl_state_t) N_storage.dummy1_allowed;
     switches[DUMMY_SWITCH_1_ID].text = "Dummy 1";
     switches[DUMMY_SWITCH_1_ID].subText = "Allow dummy 1\nin transactions";
@@ -152,11 +150,15 @@ void ui_menu_settings() {
     switches[DUMMY_SWITCH_2_ID].token = DUMMY_SWITCH_2_TOKEN;
     switches[DUMMY_SWITCH_2_ID].tuneId = TUNE_TAP_CASUAL;
 
-    nbgl_useCaseGenericSettings(APPNAME,
-                                INIT_SETTINGS_PAGE,
+    nbgl_useCaseHomeAndSettings(APPNAME,
+                                &C_app_boilerplate_64px,
+                                NULL,
+                                NULL,
                                 &settingContents,
                                 &infoList,
-                                ui_menu_main);
+                                NULL,
+                                NULL,
+                                app_quit);
 }
 
 #endif
